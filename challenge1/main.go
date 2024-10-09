c898a87786ef6476d58c474d3f21508e639935e0
Dimi3grn <dimitri.gourrin@ynov.com>
2024-10-09 15:06:46 +0200

    challenge 1 image marche pas

--- challenge1/main.go
+++ challenge1/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+type PagePromo struct {
+	Classe          string
+	Filliere        string
+	Niveau          string
+	NombreEtudiants int
+	Etudiants       []Etudiant
+}
+
+type Etudiant struct {
+	Nom    string
+	Prenom string
+	Age    int
+	Sexe   bool //true := homme
+}
+
+func main() {
+	temp, err := template.ParseGlob("./challenge1/template/*.html")
+
+	if err != nil {
+		fmt.Println(fmt.Sprint("erreur %s", err.Error()))
+		return
+	}
+	http.HandleFunc("/promo", func(w http.ResponseWriter, r *http.Request) {
+		ListeEtudiants := []Etudiant{{"gourrin", "dimitri", 17, true}, {"Xerli", "Chinois", 152, false}}
+
+		PageData := PagePromo{"B1 Informatique", "Informatique", " Bachelor 1", 45, ListeEtudiants}
+		temp.ExecuteTemplate(w, "Promo", PageData)
+
+	})
+	fileServer := http.FileServer(http.Dir("./assets"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	http.ListenAndServe("localhost:8000", nil)
+}
